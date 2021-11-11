0061ffb5e725fb46bb8a262559466bb50dcc8370
Mahmud-cse <mahmudhasan555666@gmail.com>
2021-11-12 05:28:18 +0600

    responsive

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,74 +1,113 @@
-import React from 'react';
-import { Container, Nav, Navbar, Stack, Button, NavDropdown } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
-import useAuth from '../hooks/useAuth';
-import image from '../img/logo.png';
-import './NavBar.css';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Link, NavLink } from 'react-router-dom';
+import { makeStyles } from '@mui/styles';
+import { useTheme } from '@mui/material';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
 
-const NavBar = () => {
-    const { user, logout } = useAuth();
-    return (
-        <div>
-            <Navbar collapseOnSelect expand="lg">
-                {/* container */}
-                <Container>
-                    <Navbar.Brand href="/home">
-                        <img
-                            src={image}
-                            width="50"
-                            height="50"
-                            className="d-inline-block align-top"
-                            alt="React Bootstrap logo"
-                        />
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="ms-auto">
-                            {/* stack */}
-                            <Stack direction="horizontal" gap={3}>
-                                {/* <i class="fas fa-shopping-cart"></i> */}
-                                <NavLink to="/home" className="f-13" style={{ color: "black" }} activeStyle={{
-                                    fontWeight: "bold",
-                                    color: "black"
-                                }}>Home</NavLink>
 
-                                <NavLink to="/explore" className="f-13" style={{ color: "black" }} activeStyle={{
-                                    fontWeight: "bold",
-                                    color: "black"
-                                }}>Explore</NavLink>
+export default function NavBar() {
+    const theme = useTheme();
+    const useStyle = makeStyles({
+        navItem: {
+            color: '#fff',
+            textDecoration: 'none'
+        },
+        navIcon: {
+            [theme.breakpoints.up('sm')]: {
+                display: 'none !important'
+            }
+        },
+        navItemContainer: {
+            [theme.breakpoints.down('sm')]: {
+                display: 'none !important'
+            }
+        },
+        navLogo: {
+            [theme.breakpoints.down('sm')]: {
+                textAlign: 'right !important'
+            }
+        },
+        mobileNavItem: {
+            textDecoration: 'none',
+            color: '#000'
+        }
+    })
+    const { navItem, navIcon, navItemContainer, navLogo, mobileNavItem } = useStyle();
 
-                                {
-                                    user?.email ?
-                                        <NavLink to="/dashBoard" className="f-13" style={{ color: "black" }} activeStyle={{
-                                            fontWeight: "bold",
-                                            color: "black"
-                                        }}>Dashboard</NavLink>
 
-                                        :
-                                        " "
-                                }
-                                {/* <span>{user.displayName}</span> */}
-                                {
-                                    user?.email ?
-                                        <NavLink to="" onClick={logout} className="f-13" style={{ color: "black" }} activeStyle={{
-                                            fontWeight: "bold",
-                                            color: "black"
-                                        }}>
-                                            <Button variant="dark" className="rounded-pill">LogOut({user.displayName})</Button>
-                                        </NavLink>
-                                        :
-                                        <NavLink to="/login" className="f-13" style={{ color: "black" }} activeStyle={{
-                                            fontWeight: "bold",
-                                            color: "black"
-                                        }}>Login</NavLink>
-                                }
-                            </Stack>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </div>
+    const [state, setState] = React.useState(false);
+
+    const list = (
+        <Box
+            sx={{ width: 250 }}
+            role="presentation"
+        >
+            <List>
+                <ListItem button >
+                    <ListItemText>
+                        <Link className={mobileNavItem} to="/">Home</Link>
+                    </ListItemText>
+                </ListItem>
+                <Divider />
+                <ListItem button >
+                    <ListItemText>
+                        <Link className={mobileNavItem} to="/dashBoard">Dashboard</Link>
+                    </ListItemText>
+                </ListItem>
+                <Divider />
+            </List>
+        </Box>
     );
-};
-
-export default NavBar;
+    return (
+        <>
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            sx={{ mr: 2 }}
+                            className={navIcon}
+                            onClick={() => setState(true)}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography className={navLogo} variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                            News
+                        </Typography>
+                        <Box className={navItemContainer}>
+                            <Link className={navItem} to="/"><Button color="inherit">Home</Button></Link>
+                            <Link className={navItem} to="/dashBoard"><Button color="inherit">Dashboard</Button></Link>
+                        </Box>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+            <div>
+                <React.Fragment>
+                    <Drawer
+                        open={state}
+                        onClose={() => setState(false)}
+                    >
+                        {list}
+                    </Drawer>
+                </React.Fragment>
+            </div>
+        </>
+    );
+}
